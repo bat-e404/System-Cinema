eaba70492cb86c7a5b7936f8b0d331e4b7c15f60
Bryan Aguirre <bryan_tudon@outlook.com>
2021-10-15 16:47:00 -0500

    :3

---
+++ System-Cinema/System-Cinema/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace System_Cinema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
